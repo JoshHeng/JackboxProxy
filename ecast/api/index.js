cb995bced02eaa2dd84867b79499008081fed3f3
Josh Heng <josh@joshheng.co.uk>
2021-08-19 18:26:17 +0100

    fix: Bugs

--- ecast/api/index.js
+++ ecast/api/index.js
@@ -2,10 +2,16 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
 	const { path } = req.query;
-	if (!path) res.send('Invalid path');
+	if (!path) return res.send('Invalid path');
 
-	const res2 = await axios.get(`https://ecast.jackboxgames.com${path}`).then(({data}) => data);
-	res.send({
-		...res2
-	});
+	try {
+		const res2 = await axios.get(`https://ecast.jackboxgames.com${path}`);
+		return res.send(res2.data);
+	}
+	catch (error) {
+		if (error.response) {
+			return res.status(error.response.statusCode).send(error.response.data);
+		}
+		return res.status(500).send('Invalid');
+	}
 };
