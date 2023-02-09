62a0e49d596bf7261b4e1849e9fcf6e39cfdd191
Josh Heng <josh@joshheng.co.uk>
2023-02-09 17:39:24 +0000

    Add support for bundles.jackbox.tv

--- cloudflareworker.js
+++ cloudflareworker.js
@@ -1,6 +1,7 @@
 const defaultDestination = "jackbox.tv";
 const ecastDestination = "ecast.jackboxgames.com";
 const ecastAws = "ecast-prod-28687133.us-east-1.elb.amazonaws.com";
+const bundleHost = "bundles.jackbox.tv";
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
@@ -22,18 +23,22 @@ async function handleRequest(request) {
     });
     let text = await req.text();
 
+    text = text.replace(bundleHost, `${url.hostname}/bundles`);
     text = text.replace(defaultDestination, url.hostname);
     text = text.replace(ecastDestination, url.hostname);
 
     return new Response(text, req);
   } else {
+    const bundle = url.pathname.startsWith("/bundles");
+    
     const req = await fetch(
-      `https://${defaultDestination}${url.pathname}`,
+      `https://${bundle ? bundleHost : defaultDestination}${url.pathname.slice(bundle ? 8 : 0)}`,
       request
     );
 
     if (req.headers.get("content-type") == "application/javascript") {
       let text = await req.text();
+      text = text.replace(bundleHost, `${url.hostname}/bundles`);
       text = text.replace(defaultDestination, url.hostname);
       text = text.replace(ecastDestination, url.hostname);
 
